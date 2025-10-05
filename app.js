9c68aa860d7557018defa20f99d3bdcd939ea0b8
Azmeer Hassan Ammad <azmeerhassanammad@gmail.com>
2025-10-05 07:34:03 +0500

    Created userSchema

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express =  require('express')
+const userModel = require('./models/user')
+const cookieParser = require("cookie-parser")
+
+app.set("view engine", "ejs")
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser())
 
 const app = express()
 
 app.get('/', (req, res)=>{
-    res.send("Hey");  
+    res.render('index');  
 })
 
 app.listen(3000)
