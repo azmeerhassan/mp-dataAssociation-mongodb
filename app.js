db89f770aa3532c01f8447d0875bd61e1a89b22d
Azmeer Hassan Ammad <azmeerhassanammad@gmail.com>
2025-10-05 18:12:46 +0500

    Created User Registration functionality

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express =  require('express')
 const userModel = require('./models/user')
+const postModel = require('./models/post')
 const cookieParser = require("cookie-parser")
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const app = express()
 
 app.set("view engine", "ejs")
 
@@ -8,10 +13,34 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 
-const app = express()
-
 app.get('/', (req, res)=>{
     res.render('index');  
 })
+
+app.post('/register', async(req, res)=>{
+    let {username, name, age, email, password} = req.body
+    let user =  await userModel.findOne({email})
+    if (user) res.status(500).send('User already registered!')
+
+    bcrypt.genSalt(10, (err, salt)=>{
+        bcrypt.hash(password, salt, async (err, hash)=>{
+            
+            
+           let user = await userModel.create({
+                username,
+                name, 
+                age,
+                email,
+                password: hash
+            })
+
+            let token = jwt.sign({email: email, userid: user._id}, 'shhh')
+            res.cookie("token", token)
+            res.send('registered')
+            
+        })
+        
+    })    
+} )
 
 app.listen(3000)
