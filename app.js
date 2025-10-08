c3abe43cfae881a2ce1b28171a27b8113ba7f3b2
Azmeer Hassan Ammad <azmeerhassanammad@gmail.com>
2025-10-08 21:35:44 +0500

    Profile Photo Addition

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ app.set("view engine", "ejs")
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(cookieParser())
+
 
 
 
